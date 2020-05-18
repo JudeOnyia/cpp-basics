6c3e60a42aad47c86ffe70982d73c3d5a64a958e
Jude Onyia <judeonyia10@gmail.com>
2020-05-18 15:26:42 -0700

    B-1 part f to part i (detailed description below) 1) created the multiplier, increment and modulus functions 2) created the seed function to restart the sequence generation process 3) add a member data to indicate how many next positions should be    discarded in the generated sequence. This defaults to zero 4) created the discard function to set the discard member data 5) overloaded the operator() function to advance the generator to the    next position, and skip positions that are to be discarded

--- lib/random.cpp
+++ lib/random.cpp
@@ -1,23 +1,51 @@
+#include "random.hpp"
+
 class linear_congruential_generator {
 	public:
-		typedef unsigned long long int int_type;
+		typedef unsigned long long int int_type; // type member
+		static int_type default_seed(){ return (int_type)1;} // Function to return default seed of one for all objects
 		
+		// Constructor that initializes the multiplier, increment and modulus. Seed is optional argument.
 		linear_congruential_generator(int_type a, int_type c, int_type m, int_type s = default_seed()){
 			a_ = a;
 			c_ = c;
 			m_ = m;
-			s_ = s;
+			if( (c_ % m_)==0 && (s % m_)==0 ) x_ = 1;
+			else x_ = s;
 		}
 
-		static int_type default_seed(){ return (int_type)1;}
+		int_type multiplier(){ return a_;} // Function to return multiplier value
+		int_type increment(){ return c_;} // Function to return increment value
+		int_type modulus(){ return m_;} // Function to return modulus value
 
-		int_type multiplier(){ return a_;}
-		int_type increment(){ return c_;}
+		// Function to restarts the sequence generation process with a new seed value
+		void seed(int_type s){
+			if( (c_ % m_)==0 && (s % m_)==0 ) x_ = 1;
+			else x_ = s;
+			n_ = 0;
+		}
+
+		// Function to advance the generator to the next position in the sequence
+		// with consideration to the number of positions to be discarded
+		int_type operator()(){
+			++n_;
+			do{
+				x_ = (a_ * x_ + c_) % m_;
+				--n_;
+			} while(n_);
+			return x_;
+		}
+
+		// Function to discard the next n numbers in the generated sequence
+		void discard(unsigned long long n){
+			n_ = n;
+		}
 
 
 	private:
-  		int_type a_;
-		int_type c_;
-		int_type m_;
-		int_type s_;
+  		int_type a_; // multiplier
+		int_type c_; // increment
+		int_type m_; // modulus
+		int_type x_; // current position in the generated sequence
+		unsigned long long n_ = 0; // number of positions to discard in the sequence
 }
