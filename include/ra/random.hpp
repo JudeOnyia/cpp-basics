3d58a7f5c52fe5f1cac773182784a3bba2a349e3
JudeOnyia <60678029+JudeOnyia@users.noreply.github.com>
2020-05-19 01:41:40 -0700

    Fixed the random.hpp file to have the proper declarations, and modifies the random.cpp file to have definitions of some member functions and the non-member function (operator<<).

--- include/ra/random.hpp
+++ include/ra/random.hpp
@@ -1,7 +1,49 @@
-#ifndef random_hpp
-#define random_hpp
-#include <iostream>
 namespace ra::random{
-	class linear_congruential_generator;
+	class linear_congruential_generator {
+	public:
+		typedef unsigned long long int int_type; // type member
+		static int_type default_seed(){ return (int_type)1;} // Function to return default seed of one for all objects
+		
+		// Constructor that initializes the multiplier, increment and modulus. Seed is optional argument.
+		linear_congruential_generator(int_type a, int_type c, int_type m, int_type s = default_seed());
+
+		int_type multiplier(){ return a_;} // Function to return multiplier value
+		int_type increment(){ return c_;} // Function to return increment value
+		int_type modulus(){ return m_;} // Function to return modulus value
+		int_type position(){ return x_;} // Function to return the current position in the sequence
+
+		// Function to restarts the sequence generation process with a new seed value
+		void seed(int_type s);
+
+		// Operator to advance the generator to the next position in the sequence
+		// with consideration to the number of positions to be discarded
+		int_type operator()();
+
+		// Function to discard the next n numbers in the generated sequence
+		void discard(unsigned long long n){ n_ = n; }
+
+		int_type min(){ return c_==(int_type)0? (int_type)1 : (int_type)0; } // Function to get the smallest value 
+		int_type max(){ return m-(int_type)1; } // Funtion to get the largest value in sequence
+		
+		// Operator to test two linear_congruential_generator objects for equality
+		bool operator==(const linear_congruential_generator& objA, const linear_congruential_generator& objB){
+			return (objA.a_==objB.a_ && objA.c_==objB.c_ && objA.m_==objB.m_ && objA.x_==objB.x_);
+		}
+
+		// Operator to test two linear_congruential_generator objects for inequality
+		bool operator!=(const linear_congruential_generator& objA, const linear_congruential_generator& objB){
+		        return !(objA.a_==objB.a_ && objA.c_==objB.c_ && objA.m_==objB.m_ && objA.x_==objB.x_);
+		}
+
+
+	private:
+  		int_type a_; // multiplier
+		int_type c_; // increment
+		int_type m_; // modulus
+		int_type x_; // current position in the generated sequence
+		unsigned long long n_ = (unsigned long long)0; // number of positions to discard in the sequence
+	}
+
+	// Stream inserter
+	std::ostream& operator<<(std::ostream& outStream, const linear_congruential_generator& objA);
 }
-#endif
--- lib/random.cpp
+++ lib/random.cpp
@@ -1,13 +1,10 @@
+#include <iostream>
 #include "ra/random.hpp"
 
 namespace ra::random {
-class linear_congruential_generator {
-	public:
-		typedef unsigned long long int int_type; // type member
-		static int_type default_seed(){ return (int_type)1;} // Function to return default seed of one for all objects
-		
+
 		// Constructor that initializes the multiplier, increment and modulus. Seed is optional argument.
-		linear_congruential_generator(int_type a, int_type c, int_type m, int_type s = default_seed()){
+		linear_congruential_generator::linear_congruential_generator(int_type a, int_type c, int_type m, int_type s = default_seed()){
 			a_ = a;
 			c_ = c;
 			m_ = m;
@@ -15,13 +12,8 @@ class linear_congruential_generator {
 			else x_ = s;
 		}
 
-		int_type multiplier(){ return a_;} // Function to return multiplier value
-		int_type increment(){ return c_;} // Function to return increment value
-		int_type modulus(){ return m_;} // Function to return modulus value
-		int_type position(){ return x_;} // Function to return the current position in the sequence
-
 		// Function to restarts the sequence generation process with a new seed value
-		void seed(int_type s){
+		linear_congruential_generator::void seed(int_type s){
 			if( (c_ % m_)==(int_type)0 && (s % m_)==(int_type)0 ) x_ = (int_type)1;
 			else x_ = s;
 			n_ = (unsigned long long)0;
@@ -29,7 +21,7 @@ class linear_congruential_generator {
 
 		// Operator to advance the generator to the next position in the sequence
 		// with consideration to the number of positions to be discarded
-		int_type operator()(){
+		linear_congruential_generator::int_type operator()(){
 			++n_;
 			do{
 				x_ = (a_ * x_ + c_) % m_;
@@ -38,38 +30,9 @@ class linear_congruential_generator {
 			return x_;
 		}
 
-		// Function to discard the next n numbers in the generated sequence
-		void discard(unsigned long long n){
-			n_ = n;
+		// Stream inserter
+		std::ostream& operator<<(std::ostream& outStream, const linear_congruential_generator& objA){
+			outStream << objA.multiplier() << " " << objA.increment() << " " << objA.modulus() << " " << objA.position();
+			return outStream;
 		}
-
-		int_type min(){ return c_==(int_type)0? (int_type)1 : (int_type)0; } // Function to get the smallest value 
-		int_type max(){ return m-(int_type)1; } // Funtion to get the largest value in sequence
-		
-		// Operator to test two linear_congruential_generator objects for equality
-		bool operator==(const linear_congruential_generator& objA, const linear_congruential_generator& objB){
-			return (objA.a_==objB.a_ && objA.c_==objB.c_ && objA.m_==objB.m_ && objA.x_==objB.x_);
-		}
-
-		// Operator to test two linear_congruential_generator objects for inequality
-		bool operator!=(const linear_congruential_generator& objA, const linear_congruential_generator& objB){
-		        return !(objA.a_==objB.a_ && objA.c_==objB.c_ && objA.m_==objB.m_ && objA.x_==objB.x_);
-		}
-
-
-
-
-	private:
-  		int_type a_; // multiplier
-		int_type c_; // increment
-		int_type m_; // modulus
-		int_type x_; // current position in the generated sequence
-		unsigned long long n_ = (unsigned long long)0; // number of positions to discard in the sequence
-}
-
-// Stream inserter
-std::ostream& operator<<(std::ostream& outStream, const linear_congruential_generator& objA){
-	outStream << objA.multiplier() << " " << objA.increment() << " " << objA.modulus() << " " << objA.position();
-	return outStream;
-}
 }
