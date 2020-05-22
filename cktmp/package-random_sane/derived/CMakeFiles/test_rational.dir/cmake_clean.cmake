8424743864c5b2c92bae384c8bc58ad52339013e
Jude Onyia <judeonyia@ugls5.ece.uvic.ca>
2020-05-22 15:31:03 -0700

    Put orgin

---
+++ cktmp/package-random_sane/derived/CMakeFiles/test_rational.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_rational.dir/app/test_rational.cpp.o"
+  "test_rational"
+  "test_rational.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_rational.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cktmp/package-rational_orig/source/include/ra/random.hpp
@@ -0,0 +1,1 @@
+../../../../distribution//include/ra/random.hpp
