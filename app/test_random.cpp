7b152b90b1aae6b185214853ebc2d42a85e9389a
Jude Onyia <judeonyia@ugls5.ece.uvic.ca>
2020-05-22 14:42:38 -0700

    Removed report assigne precheck

--- app/test_random.cpp
+++ app/test_random.cpp
@@ -15,40 +15,40 @@ int main(){
 	std::linear_congruential_engine<std::uint_fast32_t,14,5,29> obj_theirs;
 	obj_mine();
 	obj_theirs();
-	cout << "lc generator object: " << obj_mine << endl;
-	cout << "lc engine current state: " << obj_theirs << endl;
+	//cout << "lc generator object: " << obj_mine << endl;
+	//cout << "lc engine current state: " << obj_theirs << endl;
 
 	// Compare their minimum and maximum
-	cout << "lc generator min value: " << obj_mine.min() << endl;
-	cout << "lc engine min value: " << obj_theirs.min() << endl;
-	cout << "lc generator max value: " << obj_mine.max() << endl;
-	cout << "lc engine max value: " << obj_theirs.max() << endl;
+	//cout << "lc generator min value: " << obj_mine.min() << endl;
+	//cout << "lc engine min value: " << obj_theirs.min() << endl;
+	//cout << "lc generator max value: " << obj_mine.max() << endl;
+	//cout << "lc engine max value: " << obj_theirs.max() << endl;
 
 	// Test constructor with seed input
 	// Test seed() member function
 	// Test operator== and operator!=
 	ra::random::linear_congruential_generator obj_mine_A(97,41,300,77);
-	cout << "lc generator object (seed must be 77): " << obj_mine_A << endl;
-	obj_mine_A.seed(259);
-	cout << "lc generator object (seed change to 259): " << obj_mine_A << endl;
+	//cout << "lc generator object (seed must be 77): " << obj_mine_A << endl;
+	//obj_mine_A.seed(259);
+	//cout << "lc generator object (seed change to 259): " << obj_mine_A << endl;
 	obj_mine_A.seed(77);
 	ra::random::linear_congruential_generator obj_mine_B(97,41,300,77);
 	ra::random::linear_congruential_generator obj_mine_C(20,58,300,77);
-	cout << "lc generator equality check (Must be true): " << (obj_mine_A==obj_mine_B) << endl;
-	cout << "lc generator equality check (Must be false): " << (obj_mine_A==obj_mine_C) << endl;
-	cout << "lc generator inequality check (Must be false): " << (obj_mine_A!=obj_mine_B) << endl;
-	cout << "lc generator inequality check (Must be true): " << (obj_mine_A!=obj_mine_C) << endl;
+	//cout << "lc generator equality check (Must be true): " << (obj_mine_A==obj_mine_B) << endl;
+	//cout << "lc generator equality check (Must be false): " << (obj_mine_A==obj_mine_C) << endl;
+	//cout << "lc generator inequality check (Must be false): " << (obj_mine_A!=obj_mine_B) << endl;
+	//cout << "lc generator inequality check (Must be true): " << (obj_mine_A!=obj_mine_C) << endl;
 
 	// Test the discard member function
 	for(int i=0; i<90; ++i){
 		obj_mine_A();
 	}
 	obj_mine_B.discard(90);
-	cout << "lc generator discard function check (Must be true): " << (obj_mine_A()==obj_mine_B()) << endl;
+	//cout << "lc generator discard function check (Must be true): " << (obj_mine_A()==obj_mine_B()) << endl;
 
 	// Test condition when increment and seed are both zero
 	ra::random::linear_congruential_generator obj_mine_D(20,0,300,0);
-	cout << "lc generator seed (Must be 1): " << obj_mine_D << endl;
+	//cout << "lc generator seed (Must be 1): " << obj_mine_D << endl;
 	
 	return 0;
 
--- app/test_rational.cpp
+++ app/test_rational.cpp
@@ -9,113 +9,113 @@ int main(){
 	using std::endl;
 
 	ra::math::rational<double> obj_A;
-	cout << "1) Test default constructor" << endl;
-	cout << "   Numerator: " << obj_A.numerator() << endl;
-	cout << "   Denominator: " << obj_A.denominator() << endl << endl;
+	//cout << "1) Test default constructor" << endl;
+	//cout << "   Numerator: " << obj_A.numerator() << endl;
+	//cout << "   Denominator: " << obj_A.denominator() << endl << endl;
 
 	ra::math::rational<float> obj_B(-56);
-	cout << "2) Test constructor with single parameter" << endl;
-	cout << "   Numerator: " << obj_B.numerator() << endl;
-	cout << "   Denominator: " << obj_B.denominator() << endl << endl;
+	//cout << "2) Test constructor with single parameter" << endl;
+	//cout << "   Numerator: " << obj_B.numerator() << endl;
+	//cout << "   Denominator: " << obj_B.denominator() << endl << endl;
 
 	ra::math::rational<double> obj_C(31488,117);
-	cout << "3) Test constructor with double parameter and trunction function" << endl;
-	cout << "   Numerator: " << obj_C.numerator() << endl;
-	cout << "   Denominator: " << obj_C.denominator() << endl;
-	cout << "   Truncated value: " << obj_C.truncate() << endl << endl;
+	//cout << "3) Test constructor with double parameter and trunction function" << endl;
+	//cout << "   Numerator: " << obj_C.numerator() << endl;
+	//cout << "   Denominator: " << obj_C.denominator() << endl;
+	//cout << "   Truncated value: " << obj_C.truncate() << endl << endl;
 
 	ra::math::rational<double> obj_D(48,-4);
-	cout << "4) Test is_integer function" << endl;
-	cout << "   Numerator: " << obj_D.numerator() << endl;
-	cout << "   Denominator: " << obj_D.denominator() << endl;
-	cout << "   is_integer: " << obj_D.is_integer() << endl;
-	cout << "   Numerator: " << obj_C.numerator() << endl;
-	cout << "   Denominator: " << obj_C.denominator() << endl;
-	cout << "   is_integer: " << obj_C.is_integer() << endl << endl;
+	//cout << "4) Test is_integer function" << endl;
+	//cout << "   Numerator: " << obj_D.numerator() << endl;
+	//cout << "   Denominator: " << obj_D.denominator() << endl;
+	//cout << "   is_integer: " << obj_D.is_integer() << endl;
+	//cout << "   Numerator: " << obj_C.numerator() << endl;
+	//cout << "   Denominator: " << obj_C.denominator() << endl;
+	//cout << "   is_integer: " << obj_C.is_integer() << endl << endl;
 
 	ra::math::rational<double> obj_E(0,-4);
-	cout << "5) Test the Not(!) operator" << endl;
-	cout << "   Numerator: " << obj_E.numerator() << endl;
-	cout << "   Not(!) operator: " << !obj_E << endl;
-	cout << "   Numerator: " << obj_D.numerator() << endl;
-	cout << "   Not(!) operator: " << !obj_D << endl << endl;
+	//cout << "5) Test the Not(!) operator" << endl;
+	//cout << "   Numerator: " << obj_E.numerator() << endl;
+	//cout << "   Not(!) operator: " << !obj_E << endl;
+	//cout << "   Numerator: " << obj_D.numerator() << endl;
+	//cout << "   Not(!) operator: " << !obj_D << endl << endl;
 
 	ra::math::rational<double> obj_F(-12);
-	cout << "6) Test Equality(==) operator" << endl;
-	cout << "   Must be true: " << (obj_D==obj_F) << endl;
-	cout << "   Must be false: " << (obj_F==obj_C) << endl << endl;
+	//cout << "6) Test Equality(==) operator" << endl;
+	//cout << "   Must be true: " << (obj_D==obj_F) << endl;
+	//cout << "   Must be false: " << (obj_F==obj_C) << endl << endl;
 
-	cout << "7) Test Inequality(!=) operator" << endl;
-	cout << "   Must be true: " << (obj_C!=obj_F) << endl;
-	cout << "   Must be false: " << (obj_F!=obj_D) << endl << endl;
+	//cout << "7) Test Inequality(!=) operator" << endl;
+	//cout << "   Must be true: " << (obj_C!=obj_F) << endl;
+	//cout << "   Must be false: " << (obj_F!=obj_D) << endl << endl;
 
-	cout << "8) Test Less than(<) operator" << endl;
-	cout << "   Must be true: " << (obj_F<obj_C) << endl;
-	cout << "   Must be false: " << (obj_E<obj_F) << endl;
-	cout << "   Must be false: " << (obj_F<obj_D) << endl << endl;
+	//cout << "8) Test Less than(<) operator" << endl;
+	//cout << "   Must be true: " << (obj_F<obj_C) << endl;
+	//cout << "   Must be false: " << (obj_E<obj_F) << endl;
+	//cout << "   Must be false: " << (obj_F<obj_D) << endl << endl;
 
-	cout << "9) Test Greater than(>) operator" << endl;
-	cout << "   Must be false: " << (obj_F>obj_C) << endl;
-	cout << "   Must be true: " << (obj_E>obj_F) << endl;
-	cout << "   Must be false: " << (obj_F>obj_D) << endl << endl;
+	//cout << "9) Test Greater than(>) operator" << endl;
+	//cout << "   Must be false: " << (obj_F>obj_C) << endl;
+	//cout << "   Must be true: " << (obj_E>obj_F) << endl;
+	//cout << "   Must be false: " << (obj_F>obj_D) << endl << endl;
 
-	cout << "10) Test Less than or equals to(<=) operator" << endl;
-	cout << "    Must be true: " << (obj_F<=obj_C) << endl;
-	cout << "    Must be false: " << (obj_E<=obj_F) << endl;
-	cout << "    Must be true: " << (obj_F<=obj_D) << endl << endl;
+	//cout << "10) Test Less than or equals to(<=) operator" << endl;
+	//cout << "    Must be true: " << (obj_F<=obj_C) << endl;
+	//cout << "    Must be false: " << (obj_E<=obj_F) << endl;
+	//cout << "    Must be true: " << (obj_F<=obj_D) << endl << endl;
 
-	cout << "11) Test Greater than or equals to(>=) operator" << endl;
-	cout << "    Must be false: " << (obj_F>=obj_C) << endl;
-	cout << "    Must be true: " << (obj_E>=obj_F) << endl;
-	cout << "    Must be true: " << (obj_F>=obj_D) << endl << endl;
+	//cout << "11) Test Greater than or equals to(>=) operator" << endl;
+	//cout << "    Must be false: " << (obj_F>=obj_C) << endl;
+	//cout << "    Must be true: " << (obj_E>=obj_F) << endl;
+	//cout << "    Must be true: " << (obj_F>=obj_D) << endl << endl;
 
-	cout << "12) Test Reduced form and negative denominator" << endl;
-	cout << "    obj_C(31488,117): " << obj_C.numerator() << ", " << obj_C.denominator() << endl;
-	cout << "    obj_D(48,-4): " << obj_D.numerator() << ", " << obj_D.denominator() << endl << endl;
+	//cout << "12) Test Reduced form and negative denominator" << endl;
+	//cout << "    obj_C(31488,117): " << obj_C.numerator() << ", " << obj_C.denominator() << endl;
+	//cout << "    obj_D(48,-4): " << obj_D.numerator() << ", " << obj_D.denominator() << endl << endl;
 
 	ra::math::rational<double> obj_G(-9,0);
-	cout << "13) Test Condition when denominator is zero" << endl;
-	cout << "    obj_G(-9,0): " << obj_G.numerator() << ", " << obj_G.denominator() << endl << endl;
+	//cout << "13) Test Condition when denominator is zero" << endl;
+	//cout << "    obj_G(-9,0): " << obj_G.numerator() << ", " << obj_G.denominator() << endl << endl;
 
-	cout << "14) Test Prefix Increment(++obj) and Decrement(--obj) operators" << endl;
-	cout << "    obj_D: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    increment: " << (++obj_D).numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    decrement: " << (--obj_D).numerator() << ", " << obj_D.denominator() << endl << endl;
+	//cout << "14) Test Prefix Increment(++obj) and Decrement(--obj) operators" << endl;
+	//cout << "    obj_D: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    increment: " << (++obj_D).numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    decrement: " << (--obj_D).numerator() << ", " << obj_D.denominator() << endl << endl;
 
-	cout << "15) Test Postfix Increment(obj++) and Decrement(obj--) operators" << endl;
-	cout << "    obj_D: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    increment: " << (obj_D++).numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    See change after: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    decrement: " << (obj_D--).numerator() << ", " << obj_D.denominator() << endl;
-	cout << "    See change after: " << obj_D.numerator() << ", " << obj_D.denominator() << endl << endl;
+	//cout << "15) Test Postfix Increment(obj++) and Decrement(obj--) operators" << endl;
+	//cout << "    obj_D: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    increment: " << (obj_D++).numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    See change after: " << obj_D.numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    decrement: " << (obj_D--).numerator() << ", " << obj_D.denominator() << endl;
+	//cout << "    See change after: " << obj_D.numerator() << ", " << obj_D.denominator() << endl << endl;
 	
 	ra::math::rational<double> obj_H(-9.776,1.33);
-	cout << "16) Test case where a decimal points is used for the numerator and denominator " << endl;
-	cout << "    obj_H: " << obj_H.numerator() << ", " << obj_H.denominator() << endl << endl;
+	//cout << "16) Test case where a decimal points is used for the numerator and denominator " << endl;
+	//cout << "    obj_H: " << obj_H.numerator() << ", " << obj_H.denominator() << endl << endl;
 	
 	ra::math::rational<float> obj_I(8,10);
 	ra::math::rational<float> obj_J(1,5);
 	ra::math::rational<float> obj_K(2,3);
-	cout << "17) Test Operator(+=) and (-=) and (*=) and (/=)" << endl;
-	cout << "    (8/10) += (1/5): " << (obj_I+=obj_J).numerator() << "/" << obj_I.denominator() << endl;
-	cout << "    (prev ans) -= (2/3): " << (obj_I-=obj_K).numerator() << "/" << obj_I.denominator() << endl;
-	cout << "    (prev ans) *= (2/3): " << (obj_I*=obj_K).numerator() << "/" << obj_I.denominator() << endl;
-	cout << "    (prev ans) /= (1/5): " << (obj_I/=obj_J).numerator() << "/" << obj_I.denominator() << endl << endl;
+	//cout << "17) Test Operator(+=) and (-=) and (*=) and (/=)" << endl;
+	//cout << "    (8/10) += (1/5): " << (obj_I+=obj_J).numerator() << "/" << obj_I.denominator() << endl;
+	//cout << "    (prev ans) -= (2/3): " << (obj_I-=obj_K).numerator() << "/" << obj_I.denominator() << endl;
+	//cout << "    (prev ans) *= (2/3): " << (obj_I*=obj_K).numerator() << "/" << obj_I.denominator() << endl;
+	//cout << "    (prev ans) /= (1/5): " << (obj_I/=obj_J).numerator() << "/" << obj_I.denominator() << endl << endl;
 
-	cout << "18) Test Unary minus(-) and Unary plus(+)" << endl;
-	cout << "    obj_H: " << obj_H.numerator() << ", " << obj_H.denominator() << endl;
-	cout << "    Unary Plus: " << (+obj_H).numerator() << "/" << obj_H.denominator() << endl;
-	cout << "    Unary Minus: " << (-obj_H).numerator() << "/" << obj_H.denominator() << endl;
+	//cout << "18) Test Unary minus(-) and Unary plus(+)" << endl;
+	//cout << "    obj_H: " << obj_H.numerator() << ", " << obj_H.denominator() << endl;
+	//cout << "    Unary Plus: " << (+obj_H).numerator() << "/" << obj_H.denominator() << endl;
+	//cout << "    Unary Minus: " << (-obj_H).numerator() << "/" << obj_H.denominator() << endl;
 
-	cout << "19) Test Binary operators (+), (-), (*), and (/)" << endl;
-	cout <<"     "<<obj_I<<" + "<<obj_J<< " = " <<(obj_I+obj_J)<<endl;
-	cout <<"     "<<obj_I<<" - "<<obj_J<< " = " <<(obj_I-obj_J)<<endl;
-	cout <<"     "<<obj_I<<" * "<<obj_J<< " = " <<(obj_I*obj_J)<<endl;
-	cout <<"     "<<obj_I<<" / "<<obj_J<< " = " <<(obj_I/obj_J)<<endl<<endl;
+	//cout << "19) Test Binary operators (+), (-), (*), and (/)" << endl;
+	//cout <<"     "<<obj_I<<" + "<<obj_J<< " = " <<(obj_I+obj_J)<<endl;
+	//cout <<"     "<<obj_I<<" - "<<obj_J<< " = " <<(obj_I-obj_J)<<endl;
+	//cout <<"     "<<obj_I<<" * "<<obj_J<< " = " <<(obj_I*obj_J)<<endl;
+	//cout <<"     "<<obj_I<<" / "<<obj_J<< " = " <<(obj_I/obj_J)<<endl<<endl;
 
-	cout << "20) Test Stream extractor" << endl;
-	std::cin >> obj_K;
-	cout << obj_K << endl;
+	//cout << "20) Test Stream extractor" << endl;
+	//std::cin >> obj_K;
+	//cout << obj_K << endl;
 	
 	
 	
