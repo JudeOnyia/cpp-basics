90ad0405d8a75d7dad0959550eea554810bcc360
Jude Onyia <judeonyia@ugls8.ece.uvic.ca>
2020-05-26 12:37:17 -0700

    Chaneged CMakelist

---
+++ tmp/CMakeFiles/random.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/judeonyia/Documents/ECE596C_Assignments/ECE596C_Assgn_1/Trial/cpp_basics-JudeOnyia/app/test_random.cpp" "/home/judeonyia/Documents/ECE596C_Assignments/ECE596C_Assgn_1/Trial/cpp_basics-JudeOnyia/tmp/CMakeFiles/random.dir/app/test_random.cpp.o"
+  "/home/judeonyia/Documents/ECE596C_Assignments/ECE596C_Assgn_1/Trial/cpp_basics-JudeOnyia/lib/random.cpp" "/home/judeonyia/Documents/ECE596C_Assignments/ECE596C_Assgn_1/Trial/cpp_basics-JudeOnyia/tmp/CMakeFiles/random.dir/lib/random.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
